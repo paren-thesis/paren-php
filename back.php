5c1929e81842d63bed3bcc25a662378ccb8307d3
p@ren <ntimprosper308@gmail.com>
2025-06-09 12:20:06 +0000

    Add html code to back.php

--- back.php
+++ back.php
@@ -1,4 +1,21 @@
 <?php
+echo'<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="back.php" method="post">
+        <input type="text" name="name" id="" placeholder="Enter Name"> <br>
+        <input type="email" name="email" id="" placeholder="Enter Email"> <br>
+        <input type="text" name="age" id="" placeholder="Enter age"><br>
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>';
+
 echo $_POST['name'];
 echo "<br>";
 echo $_POST['email'];
