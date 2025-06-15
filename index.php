cfab98a868df6e0567fd1d6b49e1c228d37001ef
p@ren <ntimprosper308@gmail.com>
2025-06-15 15:24:49 +0000

    Check out comments in php (multiline and single line)

--- index.php
+++ index.php
@@ -11,5 +11,15 @@
         echo "<p>One billon dollar project ahead </p>";
         echo "Today's date is " . date("Y-m-d");
     ?>
+
+    <?php
+    echo "Comments in php";
+    // This is a comment in php
+    # This is also a comment
+    /* This
+    * is 
+    * a 
+    multiline comment*/
+    ?>
 </body>
 </html>
