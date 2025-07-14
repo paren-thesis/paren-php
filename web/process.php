1e39c4615f43a935a3cc60218fff1d6367b1eb61
p@ren <ntimprosper308@gmail.com>
2025-07-14 10:05:53 +0000

    make changes to process.php

--- web/process.php
+++ web/process.php
@@ -2,9 +2,8 @@
 
 session_start();
 require_once("database.php");
+$_SESSION["errors"] = [];
 
-
-$error[] = "";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 $name = trim($_POST['name']);
@@ -17,68 +16,47 @@ $car_comment = trim($_POST['car_comment']);
 
 
 if (empty($name)) {
-    echo "<span style='color: red'>Name field is empty</span>";
+    $_SESSION["errors"]["name"] = "Name field is empty</span>";
 } else if (!preg_match('/^[a-zA-Z]+$/', $name)) {
-    echo "<span style='color: red'>Name must be letters only</span>";
-} else {
-    echo $name;
+    $_SESSION["errors"]["name"] = "Name must be letters only</span>";
 }
 
-echo '<br>';
-
-echo $offer;
-
-echo '<br>';
 
 if (empty($email)) {
-    echo "<span style='color: red'>Email field is empty</span>";
+    $_SESSION["errors"]["email"] = "Email field is empty</span>";
 } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    echo "<span style='color: red'>Invalid email</span>";
-} else {
-    echo $email;
+    $_SESSION["errors"]["email"] = "Invalid email</span>";
 }
-
-echo '<br>';
 
 if (empty($contact)) {
-    echo "<span style='color: red'>Contact field is empty</span>";
+    $_SESSION["errors"]["contact"] = "Contact field is empty</span>";
 } else if (!preg_match('/^\d{10}+$/', $contact)) {
-    echo "<span style='color: red'>Contact must contain only 10 digits</span>";
-} else {
-    echo $contact;
+    $_SESSION["errors"]["contact"] = "Contact must contain only 10 digits</span>";
 }
-
-echo "<br>";
 
 
 if (!empty($pickup) && !empty($return_date)) {
     $pickup_date = strtotime($pickup);
     $return_date_val = strtotime($return_date);
     if ($return_date_val < $pickup_date) {
-        echo "<span style='color: red'>Return date cannot be earlier than pickup date</span><br>";
-    } else {
-        echo $pickup;
-        echo "<br>";
-        echo $return_date;
+        $_SESSION["errors"]["date"] = "Return date cannot be earlier than pickup date</span><br>";
     }
 } else {
     if (empty($return_date)) {
-        echo "Return date is empty";
+        $_SESSION["errors"]["date"] = "Return date is empty";
     }
 
     if (empty($pickup)) {
-        echo "Pickup Date is empty";
+        $_SESSION["errors"]["date"] = "Pickup Date is empty";
     }
 }
 
-echo "<br>";
-
 if (empty($car_comment)) {
-    echo "<span style='color: red'>Comment section is empty</span>";
-} else {
-    echo $car_comment;
+    $_SESSION["errors"]["name"] = "Comment section is empty</span>";
 }
 
-
 $db = new Database("localhost", "root", "" ,"car_rentals");
+$db->storeCarRentalData($name, $email, $offer, $contact, $pickup, $return_date, $car_comment);
+header("Location: index.php");
+exit;
 }
